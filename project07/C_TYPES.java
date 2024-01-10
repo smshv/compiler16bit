4ff913199b9c30ab03928b1300b07ccd736c34d6
shohaibMahmud <shohaib.mahmud@gmail.com>
2024-01-09 22:04:59 -0500

    Delete proj7 folder

--- project07/C_TYPES.java
+++
@@ -1,7 +0,0 @@
-package vmTranslator;
-
-public class C_TYPES {
-    protected  static final short C_ARITHMETIC = 0,  C_PUSH = 1, C_POP = 2, C_LABEL = 3,
-            C_GOTO = 4, C_IF = 5, C_FUNCTION = 6, C_RETURN = 7, C_CALL = 8, C_INVALID = 9,
-            C_EMPTY = 10;
-}
